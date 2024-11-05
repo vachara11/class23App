80bcd62202d707c66ccffccaf56c3eb3b95a4047
Watchara Ketkaew <wk996110@gmail.com>
2024-11-05 14:03:25 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ProjectClass23',
+  webDir: 'www'
+};
+
+export default config;
